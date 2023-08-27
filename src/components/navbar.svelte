f56195198d068111f735e610f512694194b32538
Marco D. Mosna <mmosna404@gmail.com>
2023-08-27 17:56:16 +0800

    style: new navbar and main section background

---
+++ src/components/navbar.svelte
@@ -0,0 +1,36 @@
+<div class="container">
+    <span>CATAPULT</span>
+    <nav>
+        <ul class="nav-items">
+            <li><a href="#top">Contacts</a></li>
+            <li><a href="#top">About</a></li>
+            <li><a href="#top">Product List</a></li>
+        </ul>
+    </nav>
+</div>
+
+<style>
+    .container, .nav-items {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        color: white;
+    }
+    .container {
+        padding: 20px;
+        margin-inline: 20%;
+    }
+    .nav-items {
+        gap: 20px;
+        list-style-type: none;
+    }
+    .nav-items a {
+        text-decoration: none;
+        transition: all 0.2s;
+        color: white;
+    }
+    .nav-items a:hover {
+        color: rgba(255, 255, 255, 0.616);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Navbar from "../components/navbar.svelte";
+</script>
+
+<!-- Fix this shit, so messy... -->
+<main>
+    <div class="navbar">
+        <Navbar/>
+    </div>
+    <div class="hero">
+
+    </div>
+</main>
+<footer>
+    this is a footer
+</footer>
+
+<style>
+    main {
+        font-size: 16px;
+        height: 100vh;
+        background-image: url('images/bread.png');
+    }
+    .navbar {
+       background-color: rgb(59, 59, 59);
+    }
+</style>
